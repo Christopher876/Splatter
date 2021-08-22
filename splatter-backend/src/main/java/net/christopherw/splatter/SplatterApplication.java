80561ec02496136a33f24f9750f85c3b80ba1aa9
Christopher Williams <christopherdavidwilliams21@gmail.com>
2021-08-22 13:36:02 -0400

    Initial Commit

---
+++ splatter-backend/src/main/java/net/christopherw/splatter/SplatterApplication.java
@@ -0,0 +1,13 @@
+package net.christopherw.splatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplatterApplication.class, args);
+	}
+
+}
---
+++ splatter-backend/src/test/java/net/christopherw/splatter/SplatterApplicationTests.java
@@ -0,0 +1,13 @@
+package net.christopherw.splatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
