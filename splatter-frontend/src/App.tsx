5592a334f9a57501198d784c166a1f608bad078e
Christopher Williams <christopherdavidwilliams21@gmail.com>
2021-08-23 10:15:18 -0400

    Add Login page
    - Add router and routes to the application
    - Add initial Create Account page
    - Add navbar component

--- splatter-frontend/src/App.tsx
+++ splatter-frontend/src/App.tsx
@@ -1,60 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import '@fontsource/roboto'
 
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-
-
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import NavBar from './components/NavBar';
+import createAccount from './components/createAccount';
 
 function App() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
-
   return (
-    <div className="App">
-      <Card className={classes.root} style={{width:'50%', margin:'2% auto'}} elevation={6}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/login" component={Login} />
+        <Route path="/createAccount" component={createAccount} />
+      </Switch>
+    </Router>
   );
 }
 
